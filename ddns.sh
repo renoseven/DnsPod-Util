881a54f5e90d0fe666cbefc3dd929e08f23948a0
RenoSeven <dev@renoseven.net>
2018-12-09 02:57:15 +0800

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
-if [ -z $1 ] || [ -z $2 ]; then
-	echo "Usage: $0 example.com www";
-	exit 1;
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+        echo "Usage: $0 example.com www";
+        exit 1;
 fi
 
-if [ -z "$(./dnspod-util.sh -i "$(cat api_id)" -k "$(cat api_key)" -o "_get_record_id $1 $2 A 默认")" ]; then
-	./dnspod-util.sh -i "$(cat api_id)" -k "$(cat api_key)" -o "add $1 $2 A 默认 0.0.0.0" --nologo;
+record_id=$(./dnspod-util.sh -t "$(cat api_token)" -o "getid $1 $2 A 默认");
+if [ -z "$record_id" ]; then
+        ./dnspod-util.sh -t "$(cat api_token)" -o "add $1 $2 A 默认 0.0.0.0" --nologo;
 fi
-./dnspod-util.sh -i "$(cat api_id)" -k "$(cat api_key)" -o "ddns $1 $2 A 默认" --nologo;
+
+if [ $? -eq 0 ]; then
+        ./dnspod-util.sh -t "$(cat api_token)" -o "ddns $1 $2 A 默认" --nologo;
+fi
